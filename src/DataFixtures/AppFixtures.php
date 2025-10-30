862891a3a86679b58edc94b64b295fd11d380cc1
DaliTesfaye <112868614+DaliTesfaye@users.noreply.github.com>
2025-10-30 14:49:35 +0100

    Fake Data Created using fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,17 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use App\Entity\Product;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product = new Product();
+        $product->setName("Iphone 14");
+        $product->setDescription("Apple iPhone 14 with advanced features and sleek design.");
+        $product->setSize(10);
+
+        $manager->persist($product);
 
         $manager->flush();
     }
